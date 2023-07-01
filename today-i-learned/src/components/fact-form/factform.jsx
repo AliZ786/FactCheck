43794797946475f3f5128911a23652d73c70fcbe
Mohammad Ali Zahir <m.alizahir786@gmail.com>
2023-07-01 07:09:44 -0400

    Fixed some more styling and finished fact form component

--- today-i-learned/src/components/fact-form/factform.jsx
+++ today-i-learned/src/components/fact-form/factform.jsx
@@ -1,6 +1,22 @@
 import "./../../style.css";
 
+const CATEGORIES = [
+  { name: "technology", color: "#1e1b4b" },
+  { name: "science", color: "#9f1239" },
+  { name: "finance", color: "#fb923c" },
+  { name: "society", color: "#3f6212" },
+  { name: "entertainment", color: "#0c4a6e" },
+  { name: "health", color: "#6b21a8" },
+  { name: "history", color: "#2563eb" },
+  { name: "news", color: "#450a0a" },
+  { name: "sports", color: "#075985" },
+];
+
 export default function NewFactForm() {
+  const sortedCategories = CATEGORIES.sort((a, b) =>
+    a.name.localeCompare(b.name)
+  );
+
   return (
     <form className="fact-form">
       <input type="text" placeholder="Share a fact with the world" />
@@ -8,15 +24,11 @@ export default function NewFactForm() {
 
       <select name="" id="">
         <option value="">Choose a Category</option>
-        <option value="entertainment">Entertainment</option>
-        <option value="finance">Finance</option>
-        <option value="health">Health</option>
-        <option value="history">History</option>
-        <option value="news">News</option>
-        <option value="science">Science</option>
-        <option value="society">Society</option>
-        <option value="sports">Sports</option>
-        <option value="technology">Technology</option>
+        {sortedCategories.map((category) => (
+          <option key={category.name} value={category.name}>
+            {category.name}
+          </option>
+        ))}
       </select>
       <button className="btn post-btn"></button>
     </form>
