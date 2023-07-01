8fec93221dbfa07e8a0c8909da908fd6f3a52366
Mohammad Ali Zahir <m.alizahir786@gmail.com>
2023-07-01 05:19:32 -0400

    Added some files to the react dom

--- today-i-learned/src/App.js
+++ today-i-learned/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "./style.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header className="header">
+      <div className="logo">
+        <img
+          src="logo.png"
+          height="100"
+          width="100 "
+          alt="Logo for the website"
+        />
+        <h1>Today I Learned!</h1>
+      </div>
+      <button className="btn share-btn btn-open">Share a fact</button>
+    </header>
   );
 }
 
